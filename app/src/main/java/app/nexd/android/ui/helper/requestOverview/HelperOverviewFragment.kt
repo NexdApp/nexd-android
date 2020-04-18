a2edcaa065f7044a3287e2d27855b77ddd0fafde
Claudius <privat@claudiuskienle>
2020-04-18 08:24:49 +0200

    update helper overview

--- app/src/main/java/app/nexd/android/ui/helper/requestOverview/HelperOverviewFragment.kt
+++ app/src/main/java/app/nexd/android/ui/helper/requestOverview/HelperOverviewFragment.kt
@@ -1,6 +1,5 @@
 package app.nexd.android.ui.helper.requestOverview
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
@@ -9,20 +8,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import app.nexd.android.R
 import app.nexd.android.api.model.HelpRequest
 import app.nexd.android.databinding.FragmentHelperRequestOverviewBinding
+import app.nexd.android.ui.common.DefaultSnackbar
 import app.nexd.android.ui.common.HelpRequestBinder
 import app.nexd.android.ui.dialog.SelectTextDialog
 import app.nexd.android.ui.helper.requestOverview.HelperOverviewFragmentDirections.Companion.requestDetailAction
+import app.nexd.android.ui.helper.requestOverview.HelperOverviewViewModel.Progress.*
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_helper_request_overview.*
 import mva2.adapter.ListSection
 import mva2.adapter.MultiViewAdapter
@@ -31,8 +30,9 @@ class HelperOverviewFragment : Fragment() {
 
     private val viewModel: HelperOverviewViewModel by viewModels()
     private lateinit var binding: FragmentHelperRequestOverviewBinding
-    private lateinit var nearRequestsAdapter: MultiViewAdapter
-    private lateinit var acceptedRequestsAdapter: MultiViewAdapter
+
+    private val nearRequestsAdapter = MultiViewAdapter()
+    private val acceptedRequestsAdapter = MultiViewAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,48 +47,80 @@ class HelperOverviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        acceptedRequestsAdapter = MultiViewAdapter()
         recyclerView_acceptedRequests.layoutManager = LinearLayoutManager(context)
         recyclerView_acceptedRequests.adapter = acceptedRequestsAdapter
-
-        nearRequestsAdapter = MultiViewAdapter()
-        recyclerView_nearRequests.layoutManager = LinearLayoutManager(context)
-        recyclerView_nearRequests.adapter = nearRequestsAdapter
-
-
         acceptedRequestsAdapter.registerItemBinders(
             HelpRequestBinder()
         )
+
+        recyclerView_nearRequests.layoutManager = LinearLayoutManager(context)
+        recyclerView_nearRequests.adapter = nearRequestsAdapter
         nearRequestsAdapter.registerItemBinders(
             HelpRequestBinder()
         )
 
         viewModel.run {
-            getMyAcceptedRequests().observe(viewLifecycleOwner, Observer { myAcceptedRequests ->
-
-                updateAcceptedRequests(myAcceptedRequests)
-
-                button_shopping.isEnabled = myAcceptedRequests.isNotEmpty()
+            progress.observe(viewLifecycleOwner, Observer { progress ->
+                when (progress) {
+                    is Idle -> {
+                        progressBar.visibility = View.GONE
+                    }
+                    is Loading -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                    is ZipCodeDialog -> {
+                        context?.let { context ->
+                            SelectTextDialog(
+                                context,
+                                getString(R.string.helper_request_overview_button_filter_zip),
+                                progress.zipCode
+                            )
+                                .setConfirmButton {
+                                    viewModel.filterbyZipCode(it as String)
+                                }
+                                .show()
+                                .window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+                        }
+                    }
+                    is Error -> {
+                        progressBar.visibility = View.GONE
+                        DefaultSnackbar(view, progress.message, Snackbar.LENGTH_SHORT)
+                    }
+                }
             })
 
-            getOtherOpenRequests().observe(viewLifecycleOwner, Observer { requests ->
-//                val nearTitle =
-//                    context?.getString(R.string.helper_request_overview_heading_available_section)
-//                        ?: ""
-//                val nearInfo = getString(R.string.helper_request_overview_heading_open_section_zip)
-//                val near = SpannableString(nearTitle + nearInfo)
-//                near.setSpan(
-//                    RelativeSizeSpan(0.5f), nearTitle.length,
-//                    nearTitle.length + nearInfo.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-//                )
-//                button_nearRequests.text = near
+            myAcceptedRequests.observe(viewLifecycleOwner, Observer { acceptedRequests ->
+                acceptedRequestsAdapter.removeAllSections()
 
-                updateNearbyOpenRequests(requests)
+                val acceptedRequestsList = ListSection<HelpRequest>()
+
+                acceptedRequestsList.addAll(acceptedRequests)
+                acceptedRequestsList.setOnSelectionChangedListener { request: HelpRequest,
+                                                                     b: Boolean, _ ->
+                    if (b) {
+                        request.id?.let {
+                            findNavController().navigate(requestDetailAction(it))
+                        }
+                    }
+                }
+                acceptedRequestsAdapter.addSection(acceptedRequestsList)
             })
-        }
 
-        button_nearRequests.setOnClickListener {
-            zipCodeDialog()
+            openRequests.observe(viewLifecycleOwner, Observer { requests ->
+                nearRequestsAdapter.removeAllSections()
+
+                val nearRequestList = ListSection<HelpRequest>()
+                nearRequestList.addAll(requests)
+                nearRequestList.setOnSelectionChangedListener { helpRequest: HelpRequest,
+                                                                b: Boolean, _ ->
+                    if (b) {
+                        helpRequest.id?.let {
+                            findNavController().navigate(requestDetailAction(it))
+                        }
+                    }
+                }
+                nearRequestsAdapter.addSection(nearRequestList)
+            })
         }
 
         button_shopping.setOnClickListener {
@@ -102,61 +134,8 @@ class HelperOverviewFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        viewModel.reloadData()
-    }
-
-    private fun updateAcceptedRequests(acceptedRequests: List<HelpRequest>) {
-        acceptedRequestsAdapter.removeAllSections()
-
-        val acceptedRequestsList = ListSection<HelpRequest>()
-
-        acceptedRequestsList.addAll(acceptedRequests)
-        acceptedRequestsList.setOnSelectionChangedListener { request: HelpRequest,
-                                                             b: Boolean, _: MutableList<HelpRequest> ->
-            if (b) {
-                request.id?.let { id ->
-                    val action =
-                        requestDetailAction(
-                            id
-                        )
-                    findNavController().navigate(action)
-                }
-            }
-        }
-        acceptedRequestsAdapter.addSection(acceptedRequestsList)
-    }
-
-    private fun updateNearbyOpenRequests(nearRequests: List<HelpRequest>) {
-        nearRequestsAdapter.removeAllSections()
-
-        val nearRequestList = ListSection<HelpRequest>()
-        nearRequestList.addAll(nearRequests)
-
-        nearRequestList.setOnSelectionChangedListener { helpRequest: HelpRequest,
-                                                        b: Boolean, _: MutableList<HelpRequest> ->
-            if (b) {
-                helpRequest.id?.let {
-                    findNavController().navigate(requestDetailAction(it))
-                }
-            }
-        }
-        nearRequestsAdapter.addSection(nearRequestList)
-    }
-
-    private fun zipCodeDialog() {
-        context?.let { context ->
-            SelectTextDialog(
-                context,
-//                getString(R.string.helper_request_overview_button_filter_zip),
-                "",
-                viewModel.zipCode ?: ""
-            )
-                .setConfirmButton {
-                    viewModel.filterbyZipCode(it as String)
-                }
-                .show()
-                .window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
-        }
+        viewModel.loadMyAcceptedRequests()
+        viewModel.loadNearOpenRequests()
     }
 
 }
--- app/src/main/java/app/nexd/android/ui/helper/requestOverview/HelperOverviewViewModel.kt
+++ app/src/main/java/app/nexd/android/ui/helper/requestOverview/HelperOverviewViewModel.kt
@@ -1,65 +1,137 @@
 package app.nexd.android.ui.helper.requestOverview
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.RelativeSizeSpan
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.MutableLiveData
+import app.nexd.android.R
 import app.nexd.android.api
 import app.nexd.android.api.model.HelpList
 import app.nexd.android.api.model.HelpRequest
 import app.nexd.android.api.model.HelpRequestStatus
+import app.nexd.android.ui.common.Constants.Companion.MAXIMAL_ACCEPTED_REQUESTS
 import app.nexd.android.ui.common.Constants.Companion.USER_ME
-import io.reactivex.BackpressureStrategy
-import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 class HelperOverviewViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val reloadAccepted = BehaviorSubject.create<Unit>()
-    private val reloadNearby = BehaviorSubject.create<Unit>()
+    sealed class Progress {
+        object Idle : Progress()
+        object Loading : Progress()
+        class ZipCodeDialog(val zipCode: String): Progress()
+        class Error(val message: String) : Progress()
+    }
 
     var zipCode: String? = null
         private set
 
-    // mb different way to get active requests??
-    fun getMyAcceptedRequests(): LiveData<List<HelpRequest>> {
-        val observable = reloadAccepted.flatMap {
-            api.helpListsControllerGetUserLists(null)
-                .map { helpLists ->
-                    helpLists.filter { it.status == HelpList.StatusEnum.ACTIVE }
-                        .maxBy { it.createdAt }?.helpRequests
-                        .orEmpty()
-                }
+    val myAcceptedRequestsTitle = MutableLiveData<SpannableString>()
+    val myAcceptedRequests = MutableLiveData(emptyList<HelpRequest>())
+
+    val openRequestsTitle = MutableLiveData<SpannableString>()
+    val openRequests = MutableLiveData(emptyList<HelpRequest>())
+
+    val progress = MutableLiveData<Progress>(Progress.Idle)
+
+    init {
+        loadMyAcceptedRequests()
+        loadNearOpenRequests()
+        with(api) {
+            userControllerFindMe()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    filterbyZipCode(it.zipCode)
+                }, {
+
+                })
         }
-        return LiveDataReactiveStreams.fromPublisher(observable.toFlowable(BackpressureStrategy.BUFFER))
     }
 
-    fun getOtherOpenRequests(): LiveData<List<HelpRequest>> {
-        val observable =
-            api.userControllerFindMe()
-                .flatMap { currentUser ->
-                    zipCode = currentUser.zipCode
-                    reloadNearby.flatMap { // get all requests created by other people
-                        api.helpRequestsControllerGetAll(
-                            USER_ME,
-                            true,
-                            listOf(zipCode, ""),
-                            true,
-                            listOf(
-                                HelpRequestStatus.PENDING
-                            )
+    // mb different way to get active requests??
+    @SuppressLint("CheckResult")
+    fun loadMyAcceptedRequests() {
+        progress.value = Progress.Loading
+        api.helpListsControllerGetUserLists(null)
+            .map { helpLists ->
+                helpLists.filter { it.status == HelpList.StatusEnum.ACTIVE }
+                    .maxBy { it.createdAt }?.helpRequests
+                    .orEmpty()
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                progress.value = Progress.Idle
+
+                val acceptedTitle = getApplication<Application>().resources.getString(
+                        R.string.helper_request_overview_heading_accepted_section)
+                val acceptedInfo = getApplication<Application>().resources.getString(
+                    R.string.helper_request_overview_heading_accepted_section_counter,
+                    it.size,
+                    MAXIMAL_ACCEPTED_REQUESTS
+                )
+                val accepted = SpannableString(acceptedTitle + acceptedInfo)
+                accepted.setSpan(
+                    RelativeSizeSpan(0.5f), acceptedTitle.length,
+                    acceptedTitle.length + acceptedInfo.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                myAcceptedRequestsTitle.value = accepted
+                myAcceptedRequests.value = it
+            }, {
+                progress.value = Progress.Error(it.message.toString())
+            })
+    }
+
+    @SuppressLint("CheckResult")
+    fun loadNearOpenRequests() {
+        progress.value = Progress.Loading
+        api
+            .helpRequestsControllerGetAll(
+                USER_ME,
+                true,
+                listOf(zipCode),
+                true,
+                listOf(
+                    HelpRequestStatus.PENDING
+                )
+            )
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                progress.value = Progress.Idle
+
+                val nearTitle = getApplication<Application>()
+                    .applicationContext
+                    .getString(R.string.helper_request_overview_heading_available_section)
+                var nearSubtitle = ""
+                zipCode?.let { zipCode ->
+                    nearSubtitle = getApplication<Application>()
+                        .applicationContext
+                        .getString(
+                            R.string.helper_request_overview_heading_open_section_zip,
+                            zipCode
                         )
-                    }
                 }
-        return LiveDataReactiveStreams.fromPublisher(observable.toFlowable(BackpressureStrategy.BUFFER))
+                val near = SpannableString(nearTitle + nearSubtitle)
+                near.setSpan(
+                    RelativeSizeSpan(0.5f), nearTitle.length,
+                    nearTitle.length + nearSubtitle.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+
+                openRequestsTitle.value = near
+                openRequests.value = it
+            }, {
+                progress.value = Progress.Error(it.message.toString())
+            })
     }
 
-    fun reloadData() {
-        reloadAccepted.onNext(Unit)
-        reloadNearby.onNext(Unit)
+    fun editZipCode() {
+        progress.value = Progress.ZipCodeDialog(zipCode ?: "")
     }
 
-    fun filterbyZipCode(zipCode: String) {
-        this.zipCode = zipCode
-        reloadNearby.onNext(Unit)
+    fun filterbyZipCode(zipCode: String?) {
+        progress.value = Progress.Idle
+        this.zipCode = if (zipCode.isNullOrBlank()) null else zipCode
+        loadNearOpenRequests()
     }
 }
