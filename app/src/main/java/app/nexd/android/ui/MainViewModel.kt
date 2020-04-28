51bb85ffb4c9dbc5e42acb48ec14f5eb5a0dd019
Andre Stefanov <mail@andrestefanov.de>
2020-04-28 02:09:59 +0200

    fixed user being locked in roleFragment

--- app/src/main/java/app/nexd/android/ui/MainViewModel.kt
+++ app/src/main/java/app/nexd/android/ui/MainViewModel.kt
@@ -1,9 +1,9 @@
 package app.nexd.android.ui
 
-import androidx.lifecycle.ViewModel
-import app.nexd.android.Preferences
 import android.util.Log
 import androidx.annotation.IdRes
+import androidx.lifecycle.ViewModel
+import app.nexd.android.Preferences
 import app.nexd.android.R
 
 class MainViewModel(private val preferences: Preferences) : ViewModel() {
@@ -12,14 +12,13 @@ class MainViewModel(private val preferences: Preferences) : ViewModel() {
 
     @IdRes
     fun getNavigationDestination(): Int? {
-        return if (isAuthenticated()) {
-            if (preferences.registrationComplete) {
-                Log.v("Navigation", "redirect to roleFragment")
-                R.id.roleFragment
-            } else {
+        if (isAuthenticated()) {
+            if (!preferences.registrationComplete) {
                 Log.v("Navigation", "redirect to registerDetailedViewModel")
-                R.id.registerDetailedFragment
+                return R.id.registerDetailedFragment
             }
-        } else null
+        }
+
+        return null
     }
 }
